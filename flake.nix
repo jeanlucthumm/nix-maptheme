6e6202fb2df93082863a08e9402ec8a6b35842fa
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-12-19 20:41:17 -0800

    Improve flake.nix

--- flake.nix
+++ flake.nix
@@ -2,16 +2,30 @@
   description = "A resusable NixOS module system for mapping base16/24 colors to program-specific configurations";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
+    nixpkgs,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (
-      system: {
-        nixosModules.maptheme = ./maptheme/nixos;
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            statix # Nix linter
+            alejandra # Nix formatter
+            nil # Nix LSP
+          ];
+        };
+        formatter = pkgs.alejandra;
       }
-    );
+    )
+    // {
+      nixosModules.maptheme = ./maptheme/nixos;
+    };
 }
