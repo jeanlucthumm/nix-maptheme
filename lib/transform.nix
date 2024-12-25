cdfe0ef2ec8891ad2555bd2927a4d906100c3a48
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-12-25 14:29:24 -0800

    Make withHastag accept attrs

--- lib/transform.nix
+++ lib/transform.nix
@@ -1,4 +1,6 @@
-{...}: {
-  # Transform a hex color without a hashtag to one with a hashtag.
-  withHashtag = color: "#${color}";
+{lib, ...}: let
+  prefixHash = color: "#${color}";
+in {
+  # Transform a base16 colors scheme without hashtags to one with a hashtags.
+  withHashtags = lib.attrsets.mapAttrs (name: color: prefixHash color);
 }
