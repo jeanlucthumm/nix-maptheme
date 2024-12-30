1ed310d65880808dd4e65a4eeb74477fdac1725a
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-12-30 09:46:11 +0200

    Add bat module

---
+++ modules/hm/bat/default.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  slib = import ../../lib/types.nix {inherit lib;};
+in {
+  options.maptheme.bat = {
+    enable = lib.mkEnableOption "bat colors";
+    colors = slib.options.base16option;
+  };
+
+  config = lib.mkIf config.maptheme.bat.enable {
+    programs.bat = {
+      # This theme is reused for yazi. Changes to the template
+      # will need to be applied to modules/yazi/hm.nix
+      themes."base16-maptheme".src = config.maptheme.bat.colors {
+        template = ./base16-maptheme.mustache;
+        extension = ".tmTheme";
+      };
+
+      config.theme = "base16-maptheme";
+    };
+  };
+}
